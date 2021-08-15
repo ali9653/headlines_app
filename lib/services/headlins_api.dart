cca1cfbad530d2a38431e0d627bb5c0ff5dd6af2
mohammed123 <ali262883@gmail.com>
2021-08-15 13:49:11 +0530

    multiple api keys

--- lib/services/headlins_api.dart
+++ lib/services/headlins_api.dart
@@ -1,25 +1,33 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 import 'package:headlines_app/models/headlines.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HeadlinesApi {
-
+  static Random random = new Random();
+  // Since the free version of NewsApi has the limit of 100 requests per day, 4 api keys have been added to avoid any stoppage during the testing of the app
+  static final List<String> keys = [
+    "372c1bec5e7b4f23b678bab4a9905f01",
+    "81a01b30c5774bb68889bf62e3b038a7",
+    "9b6c3160fad54f6aa8ab9ff6ffd7cbef",
+    "e26618e15d774b558dbc0a0f7554234f"
+  ];
   static final String _baseUrl = "https://newsapi.org/v2/";
   static final String _apiKey = "0050429b837248698a431adaf52e0b5c";
 
-  static List<Article> getArticles ( String responseBody ) {
+  static List<Article> getArticles(String responseBody) {
     Map<String, dynamic> json = jsonDecode(responseBody);
     List<dynamic> body = json['articles'];
     List<Article> articles = body.map((e) => Article.fromJson(e)).toList();
     return articles;
   }
 
-
   static Future<List<Article>> getTopHeadlines(String category, int page) async {
     try {
-      var response = await http.get(Uri.parse("${_baseUrl}top-headlines?country=us&page=$page&category=$category&pageSize=15&apiKey=${_apiKey}"));
+      var response = await http.get(Uri.parse(
+          "${_baseUrl}top-headlines?country=us&page=$page&category=$category&pageSize=15&apiKey=${keys[random.nextInt(keys.length)].toString()}"));
       if (response.statusCode == 200) {
         SharedPreferences prefs = await SharedPreferences.getInstance();
         prefs.setString("headlines", response.body);
@@ -36,7 +44,8 @@ class HeadlinesApi {
 
   static Future<List<Article>> getArticlesFromSearch(String keyword, int page) async {
     try {
-      var response = await http.get(Uri.parse("${_baseUrl}everything?q=$keyword&apiKey=${_apiKey}&page=$page&pageSize=25"));
+      var response =
+          await http.get(Uri.parse("${_baseUrl}everything?q=$keyword&apiKey=${keys[random.nextInt(keys.length)].toString()}&page=$page&pageSize=25"));
       if (response.statusCode == 200) {
         return getArticles(response.body);
       } else {
